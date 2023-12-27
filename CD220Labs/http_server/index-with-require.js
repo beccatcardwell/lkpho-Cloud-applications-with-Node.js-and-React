6f1390a38a0237183c93aba5a8058c24eb91a8ac
Becca <beccatcardwell@gmail.com>
2023-12-27 17:24:11 -0500

    Added switch statement to give greetings

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -3,8 +3,31 @@ const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
+  let dateVal = today.getDate();
+  let time = dateVal.getHours()
+  let greeting = "It is certainly a time"
+
+  switch (true) {
+    case time > 6 && time < 12:
+        greeting = 'Good Morning!';
+        break;
+    case time > 12 && time < 18:
+        greeting = 'Good Afternoon!';
+        break;
+    case time > 18 && time < 20:
+        greeting = 'Good Evening!';
+        break;
+    case time > 20 && time < 24:
+        greeting = 'Good Night!';
+        break;
+    default:
+        break;
+    }
+
+  res.end(`Hello, World! ${greeting}`);
 }
+
+
 
 const port = 8080;
 const server = http.createServer(requestListener);
